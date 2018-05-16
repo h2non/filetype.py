f1846491a738efd09845457af8ed1a7e00fba779
Andrii Zhezhel <azhezhel@cloudferro.com>
2018-05-16 18:03:50 +0200

    Fixed number of readed bytes in utils.py

--- filetype/utils.py
+++ filetype/utils.py
@@ -3,46 +3,46 @@
 
 def get_signature_bytes(path):
     """
-    Reads file from disk and returns the first 256 bytes
+    Reads file from disk and returns the first 262 bytes
     of data representing the magic number header signature.
 
     Args:
         path: path string to file.
 
     Returns:
-        First 256 bytes of the file content as bytearray type.
+        First 262 bytes of the file content as bytearray type.
     """
     with open(path, 'rb') as fp:
-        return bytearray(fp.read(256))
+        return bytearray(fp.read(262))
 
 
 def signature(array):
     """
-    Returns the first 256 bytes of the given bytearray
+    Returns the first 262 bytes of the given bytearray
     as part of the file header signature.
 
     Args:
         array: bytearray to extract the header signature.
 
     Returns:
-        First 256 bytes of the file content as bytearray type.
+        First 262 bytes of the file content as bytearray type.
     """
     length = len(array)
-    index = 256 if length > 256 else length
+    index = 262 if length > 262 else length
 
     return array[:index]
 
 
 def get_bytes(obj):
     """
-    Infers the input type and reads the first 256 bytes,
+    Infers the input type and reads the first 262 bytes,
     returning a sliced bytearray.
 
     Args:
         obj: path to file, bytes or bytearray.
 
     Returns:
-        First 256 bytes of the file content as bytearray type.
+        First 262 bytes of the file content as bytearray type.
 
     Raises:
         TypeError: if obj is not a supported type.
