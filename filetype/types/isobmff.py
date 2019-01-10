ec2e78f946e3e9b80e385ea0db7a3bc3cf608efd
David Poirier <david-poirier-csn@users.noreply.github.com>
2019-01-11 10:35:26 +1100

    fix py2.7 error

--- filetype/types/isobmff.py
+++ filetype/types/isobmff.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-
 from __future__ import absolute_import
+import codecs
 
 from .base import Type
 
@@ -18,16 +18,17 @@ class IsoBmff(Type):
     def _is_isobmff(self, buf):
         if len(buf) < 16 or buf[4:8] != b'ftyp':
             return False
-        if len(buf) < int.from_bytes(buf[0:4], byteorder='big'):
+        if len(buf) < int(codecs.encode(buf[0:4], 'hex'), 16):
             return False
         return True
 
     def _get_ftyp(self, buf):
-        ftyp_len = int.from_bytes(buf[0:4], byteorder='big')
+        ftyp_len = int(codecs.encode(buf[0:4], 'hex'), 16)
         major_brand = buf[8:12].decode()
-        minor_version = int.from_bytes(buf[12:16], byteorder='big')
+        minor_version = int(codecs.encode(buf[12:16], 'hex'), 16)
         compatible_brands = []
         for i in range(16, ftyp_len, 4):
             compatible_brands.append(buf[i:i+4].decode())
 
         return major_brand, minor_version, compatible_brands
+
