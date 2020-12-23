6d4d337d6cefc76cef90155bae31dceb8923eb87
dosas <dosas@github.com>
2020-12-23 17:19:23 +0100

    Switch to pytest.

--- tests/test_filetype.py
+++ tests/test_filetype.py
@@ -2,83 +2,86 @@
 
 from __future__ import absolute_import
 
-import unittest
+import os
 
 import filetype
 
 from . import FIXTURES
 
 
-class TestFileType(unittest.TestCase):
+class TestFileType(object):
+
     def test_guess_file_path(self):
-        kind = filetype.guess(FIXTURES + '/sample.jpg')
-        self.assertTrue(kind is not None)
-        self.assertEqual(kind.mime, 'image/jpeg')
-        self.assertEqual(kind.extension, 'jpg')
+        kind = filetype.guess(os.path.join(FIXTURES, 'sample.jpg'))
+        assert kind is not None
+        assert kind.mime == 'image/jpeg'
+        assert kind.extension == 'jpg'
 
     def test_guess_buffer(self):
         buf = bytearray([0xFF, 0xD8, 0xFF, 0x00, 0x08])
         kind = filetype.guess(buf)
-        self.assertTrue(kind is not None)
-        self.assertEqual(kind.mime, 'image/jpeg')
-        self.assertEqual(kind.extension, 'jpg')
+        assert kind is not None
+        assert kind.mime == 'image/jpeg'
+        assert kind.extension == 'jpg'
 
     def test_guess_buffer_invalid(self):
         buf = bytearray([0xFF, 0x00, 0x00, 0x00, 0x00])
         kind = filetype.guess(buf)
-        self.assertTrue(kind is None)
+        assert kind is None
 
     def test_guess_memoryview(self):
         buf = memoryview(bytearray([0xFF, 0xD8, 0xFF, 0x00, 0x08]))
         kind = filetype.guess(buf)
-        self.assertTrue(kind is not None)
-        self.assertEqual(kind.mime, 'image/jpeg')
-        self.assertEqual(kind.extension, 'jpg')
+        assert kind is not None
+        assert kind.mime == 'image/jpeg'
+        assert kind.extension == 'jpg'
 
 
-class TestFileTypeExtension(unittest.TestCase):
+class TestFileTypeExtension(object):
+
     def test_guess_extension_file_path(self):
-        ext = filetype.guess_extension(FIXTURES + '/sample.jpg')
-        self.assertTrue(ext is not None)
-        self.assertEqual(ext, 'jpg')
+        ext = filetype.guess_extension(os.path.join(FIXTURES, 'sample.jpg'))
+        assert ext is not None
+        assert ext == 'jpg'
 
     def test_guess_extension_buffer(self):
         buf = bytearray([0xFF, 0xD8, 0xFF, 0x00, 0x08])
         ext = filetype.guess_extension(buf)
-        self.assertTrue(ext is not None)
-        self.assertEqual(ext, 'jpg')
+        assert ext is not None
+        assert ext == 'jpg'
 
     def test_guess_extension_buffer_invalid(self):
         buf = bytearray([0xFF, 0x00, 0x00, 0x00, 0x00])
         ext = filetype.guess_extension(buf)
-        self.assertTrue(ext is None)
+        assert ext is None
 
     def test_guess_extension_memoryview(self):
         buf = memoryview(bytearray([0xFF, 0xD8, 0xFF, 0x00, 0x08]))
         ext = filetype.guess_extension(buf)
-        self.assertTrue(ext is not None)
-        self.assertEqual(ext, 'jpg')
+        assert ext is not None
+        assert ext == 'jpg'
 
 
-class TestFileTypeMIME(unittest.TestCase):
+class TestFileTypeMIME(object):
+
     def test_guess_mime_file_path(self):
-        mime = filetype.guess_mime(FIXTURES + '/sample.jpg')
-        self.assertTrue(mime is not None)
-        self.assertEqual(mime, 'image/jpeg')
+        mime = filetype.guess_mime(os.path.join(FIXTURES, 'sample.jpg'))
+        assert mime is not None
+        assert mime == 'image/jpeg'
 
     def test_guess_mime_buffer(self):
         buf = bytearray([0xFF, 0xD8, 0xFF, 0x00, 0x08])
         mime = filetype.guess_mime(buf)
-        self.assertTrue(mime is not None)
-        self.assertEqual(mime, 'image/jpeg')
+        assert mime is not None
+        assert mime == 'image/jpeg'
 
     def test_guess_mime_buffer_invalid(self):
         buf = bytearray([0xFF, 0x00, 0x00, 0x00, 0x00])
         mime = filetype.guess_mime(buf)
-        self.assertTrue(mime is None)
+        assert mime is None
 
     def test_guess_mime_memoryview(self):
         buf = memoryview(bytearray([0xFF, 0xD8, 0xFF, 0x00, 0x08]))
         mime = filetype.guess_mime(buf)
-        self.assertTrue(mime is not None)
-        self.assertEqual(mime, 'image/jpeg')
+        assert mime is not None
+        assert mime == 'image/jpeg'
