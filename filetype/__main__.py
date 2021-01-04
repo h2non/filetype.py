5084c8d37ac9c2bb62366e99af81f7f9949906d7
yanhuihang <yanhuihang@126.com>
2021-01-04 22:49:03 +0800

    [v1.0.8] Add command line

---
+++ filetype/__main__.py
@@ -0,0 +1,26 @@
+import filetype
+
+def guess(path):
+	kind = filetype.guess(path)
+	if kind is None:
+		print('{}: File type determination failure.'.format(path))
+	else:
+		print('{}: {} ({})'.format(path, kind.extension, kind.mime))
+
+def main():
+	import argparse
+
+	parser = argparse.ArgumentParser(description='Determine type of FILEs.')
+	parser.add_argument("file", nargs='+')
+	parser.add_argument('-v', '--version', action='store_true',
+		help='output version information and exit')
+	args = parser.parse_args()
+
+	if args.version:
+		print(filetype.version)
+	else:
+		for i in args.file:
+			guess(i)
+			
+if __name__ == '__main__':
+	main()
