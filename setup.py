2777b6687158c4e007d0f3f4d870f6b47610c4fd
Pavel Savchenko <asfaltboy@gmail.com>
2021-12-19 10:14:26 +0000

    Add python 3.9 to version classifiers
    Update setup.py metadata to include version python version classifiers that we already test against

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: System',
         'Topic :: System :: Filesystems',
         'Topic :: Utilities'],
