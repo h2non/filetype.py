c61307aaf2116254eef523fb8225aecaf87e14a3
Tom <tomas@aparicio.me>
2022-04-19 22:17:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='filetype',
-    version='1.0.10',
+    version='1.0.11',
     description='Infer file type and MIME type of any file/buffer. '
                 'No external dependencies.',
     long_description=codecs.open('README.rst', 'r',
