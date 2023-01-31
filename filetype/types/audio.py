dc8c9502bf0e968c8a836d523a55f603c1123f73
CatKasha <CatKasha@users.noreply.github.com>
2023-01-31 23:59:27 +0300

    forgot return in mp3

--- filetype/types/audio.py
+++ filetype/types/audio.py
@@ -54,6 +54,8 @@ class Mp3(Type):
                     buf[1] == 0xFA or #MPEG 1 with error protection
                     buf[1] == 0xFB):  #MPEG 1 w/o error protection
                     return True
+                else:
+                    return False
             else:
                 return False
         else:
